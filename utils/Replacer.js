d4e53e7f30bfe2c86f7d9f2b6dd843a60d754f9f
Hai Nguyen <nguyenhai@telio.vn>
2021-09-04 04:09:59 +0700

    replace special character after replace token

--- utils/Replacer.js
+++ utils/Replacer.js
@@ -1,32 +1,49 @@
 const _ = require('lodash')
 
-const tag_cleaner = (tag, open_tag, close_tag) => tag.replace(open_tag, '').replace(close_tag, '')
+const tag_cleaner = (tag, open_tag, close_tag) =>
+  tag.replace(open_tag, '').replace(close_tag, '')
 
-const tags = [ 
-    {
-        tag: /\{\{(.*?)\}\}/g,
-        open_tag: /^\{\{/,
-        close_tag: /\}\}$/,
-        resolver: function(data) {
-            return (tag) => _.get(data, tag_cleaner(tag, this.open_tag, this.close_tag))
-        }
-    }
+const replaceSpecial = (text) => {
+  // preserve newlines, etc - use valid JSON
+  text = text
+    .replace(/\\n/g, '\\n')
+    .replace(/\\'/g, "\\'")
+    .replace(/"/g, '\\"')
+    .replace(/\\&/g, '\\&')
+    .replace(/\\r/g, '\\r')
+    .replace(/\\t/g, '\\t')
+    .replace(/\\b/g, '\\b')
+    .replace(/\\f/g, '\\f')
+  // remove non-printable and other non-valid JSON chars
+  text = text.replace(/[\u0000-\u0019]+/g, '')
+  return text
+}
+
+const tags = [
+  {
+    tag: /\{\{(.*?)\}\}/g,
+    open_tag: /^\{\{/,
+    close_tag: /\}\}$/,
+    resolver: function (data) {
+      return (tag) =>
+        _.get(data, tag_cleaner(tag, this.open_tag, this.close_tag))
+    },
+  },
 ]
 
 /**
- * 
+ *
  * @param {String} text the string to be processed
  * @param {Object} source_data {} object that contains the source data
  * @returns {String} processed string
  */
 const replace = (text, source_data = {}) => {
-    let new_text = text + ''
+  let new_text = text + ''
 
-    tags.forEach(ftag => {
-        new_text = new_text.replace(ftag.tag, ftag.resolver(source_data))
-    })
-
-    return new_text
+  tags.forEach((ftag) => {
+    new_text = new_text.replace(ftag.tag, ftag.resolver(source_data))
+  })
+  return replaceSpecial(new_text)
 }
 
 module.exports = replace
